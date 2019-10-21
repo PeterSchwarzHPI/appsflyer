dfcb9e6de939f2cfd2c0595ecdf4ebb7eb9be39c
Peter Schwarz <pe.schwarz@hotmail.de>
2019-10-21 17:12:52 +0200

    minor bug fix

--- appsflyer/clients.py
+++ appsflyer/clients.py
@@ -22,7 +22,7 @@ class AppsFlyerClient(object):
         result = None
         for prefix, event_class in self.prefix_event_class_mapping.items():
             if app_id.startswith(prefix):
-                self.event_class = event_class
+                result = event_class
         assert result is not None
 
     def _prepare_headers(self):
