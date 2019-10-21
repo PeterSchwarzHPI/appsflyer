2d73a19e1a47461eca23f043f2abf283162db725
Peter Schwarz <pe.schwarz@hotmail.de>
2019-10-21 17:15:05 +0200

    minor bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='appsflyer',
-    version='0.0.11',
+    version='0.0.12',
     description='AppsFlyer client for Python',
     author='PeteBlack',
     url='https://github.com/PeterSchwarzHPI/appsflyer',
