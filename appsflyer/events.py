0fe52b53cceadadad9f8920cec3468b6d622efc3
Peter Schwarz <pe.schwarz@hotmail.de>
2019-10-29 16:23:51 +0100

    use revenue events to make life easier

--- appsflyer/events.py
+++ appsflyer/events.py
@@ -1,10 +1,12 @@
 import json
 
 class AppsFlyerEvent(object):
-    def __init__(self, appsflyer_id, name, payload, customer_user_id=None, ip=None, time=None, currency=None):
+    def __init__(self, appsflyer_id, name, payload=None, idfa=None, advertising_id=None, customer_user_id=None, ip=None, time=None):
         self.appsflyer_id = appsflyer_id
         self.name = name
-        self.payload = json.dumps(str(payload)) # for formatting reasons
+        self.payload = payload
+        self.idfa = idfa
+        self.advertising_id = advertising_id
         self.customer_user_id = customer_user_id
         self.ip = ip
         self.time = time
@@ -14,36 +16,27 @@ class AppsFlyerEvent(object):
         event_json = {
             'appsflyer_id': self.appsflyer_id,
             'eventName': self.name,
-            'eventValue': self.payload or ""
+            'eventValue': json.dumps(self.payload) if self.payload else ""
         }
         if self.customer_user_id:
             event_json['customer_user_id'] = self.customer_user_id
         if self.ip:
             event_json['ip'] = self.ip
-        if self.currency:
-            event_json['eventCurrency'] = self.currency
         if self.time:
             event_json['eventTime'] = self.time
-        return event_json
-
-class IosAppsFlyerEvent(AppsFlyerEvent):
-    def __init__(self, appsflyer_id, name, payload, idfa=None, customer_user_id=None, ip=None, time=None, currency=None):
-        super(IosAppsFlyerEvent, self).__init__(appsflyer_id, name, payload, time=time, currency=currency)
-        self.idfa = idfa
-
-    def to_json(self):
-        event_json = super(IosAppsFlyerEvent, self).to_json()
         if self.idfa:
             event_json['idfa'] = self.idfa
-        return event_json
-
-class AndroidAppsFlyerEvent(AppsFlyerEvent):
-    def __init__(self, appsflyer_id, name, payload, advertising_id=None, customer_user_id=None, ip=None, time=None, currency=None):
-        super(AndroidAppsFlyerEvent, self).__init__(appsflyer_id, name, payload, time=time, currency=currency)
-        self.advertising_id = advertising_id
-
-    def to_json(self):
-        event_json = super(AndroidAppsFlyerEvent, self).to_json()
-        if self.advertising_id:
+        else self.advertising_id:
             event_json['advertising_id'] = self.advertising_id
         return event_json
+
+class RevenueEvent(AppsFlyerEvent):
+    def __init__(self, appsflyer_id, revenue, currency, idfa=None, advertising_id=None, customer_user_id=None, ip=None, time=None):
+        super(RevenueEvent, self).__init__(appsflyer_id, 'af_purchase', idfa=idfa, advertising_id=advertising_id, customer_user_id=customer_user_id, ip=ip, time=time) 
+        self.revenue = revenue
+        self.currency = currency
+
+    def to_json(self):
+        event_json = super(RevenueEvent, self).to_json()
+        event_json['eventValue'] = "{\"af_revenue\": \"$REVENUE$\"}".replace('$REVENUE$', str(self.revenue))
+        event_json['eventCurrency'] = self.currency
