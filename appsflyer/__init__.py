4359d48fd561e58b7fb8746ca82200041f4fafa5
Peter Schwarz <pe.schwarz@hotmail.de>
2020-04-06 08:24:48 +0200

    minor bug fixes

--- appsflyer/__init__.py
+++ appsflyer/__init__.py
@@ -1,9 +1,10 @@
-from .events import AppsFlyerEvent, RevenueEvent, LoginEvent
+from .events import AppsFlyerEvent, RevenueEvent, LoginEvent, RegistrationEvent
 from .clients import AppsFlyerClient
 
 __all__ = [
     'AppsFlyerEvent',
     'RevenueEvent',
     'LoginEvent',
+    'RegistrationEvent',
     'AppsFlyerClient'
 ]
